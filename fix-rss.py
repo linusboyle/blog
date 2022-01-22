c1f1b509d9034f0aa79839a96eb2e72186b81c23
Zhilei Han <linusboyle@outlook.com>
2022-01-22 13:33:56 +0800

    fix rss

---
+++ fix-rss.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# author: Zhilei Han <hzl21@mails.tsinghua.edu.cn>
+# Last Modified: 2022-01-22 12:25:23
+
+import re
+import sys
+
+def convert(x):
+    m=x.groups()[0]
+    return bytes.fromhex(m).decode('utf-16-be')
+
+f = open('blog/rss', 'r')
+content = f.read()
+f.close()
+converted = re.sub(r"<#([0-9a-fA-F]{4,4})>", convert, content)
+f = open('blog/rss', 'w')
+f.write(converted)
